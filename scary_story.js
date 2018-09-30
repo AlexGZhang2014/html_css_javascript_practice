e01631484a451040abc246d3706654b0e9776374
AlexGZhang2014 <alexgzhang92@gmail.com>
2018-09-29 22:52:46 -0400

    Finish defining all vanilla JS functions and confirm that they allow correct DOM manipulation

--- scary_story.js
+++ scary_story.js
@@ -8,11 +8,22 @@ class ScaryStory {
     e.preventDefault();
     const title = document.getElementById("title").value;
     const content = document.getElementById('content').value;
+    const newStory = { title, content };
+    ScaryStory.successSubmit(newStory);
   }
 
   static successSubmit(data) {
     let story = new ScaryStory(data);
+    story.addNewStory();
+  }
 
+  addNewStory() {
+    const title = document.createElement("h4");
+    const content = document.createElement("p");
+    title.append(this.title);
+    content.append(this.content);
+    document.getElementById('scary_stories').append(title);
+    document.getElementById('scary_stories').append(content);
   }
 
   static addJSListener() {
